f3da6b8203bd6e0a486f39a5e4c0c1b388799cbd
wesaqyte <136059883+wesaqyte@users.noreply.github.com>
2023-06-09 07:16:06 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 #请修改为自己的UUID
-export UUID=ba3f6e6d-0648-4b39-b35d-36d2355e719
+export UUID=1df09ed6-0695-11ee-be56-0242ac120002
 
 #请修改为自己设置的伪装站，不要带https://
-export ProxySite=www.holehike.com
+export ProxySite=www.bing.com
 
 #端口，如无特殊需求请勿更改,如果要改请一并修改dockerfile中的端口
 export Port=8080
